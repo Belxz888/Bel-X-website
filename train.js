9423cf9fb59f08c1060e06d31b25963dc65080b5
unknown <116820113+Belykh555@users.noreply.github.com>
2023-04-18 19:57:03 +0400

    new

--- train.js
+++ train.js
@@ -1,22 +1,16 @@
-var menu = document.getElementById("menu")
-var r=document.getElementsByClassName("r")
-function toggle_menu()
+var canvas = document.getElementById("hi");
+if (canvas.getContext) 
 {
-if(menu.style.left!="85%")
-{
-menu.style.left="85%"
-r[0].style.transform="translate(0px,13px) rotate(45deg)"
-
-r[1].style.background="rgba(0,0,0,0)"
-r[2].style.transform="translate(0px,-13px) rotate(-45deg)"
-return
-}
-if(menu.style.left != "100%")
-{
-menu.style.left="100%"
-r[0].style.transform="translate(0px,0px) rotate(0deg)"
-r[1].style.background ="rgba(240, 248, 255)"
-r[2].style.transform="translate(0px,0px) rotate(0deg)"
-return
-}
+ var ctx = canvas.getContext('2d');
+ ctx.lineWidth = "3";
+ ctx.strokeStyle = "red";
+ ctx.strokeRect(5, 5, 300, 250);
+ ctx.lineWidth = "25";
+ ctx.lineJoin = "bevel";
+ ctx.strokeStyle = "blue";
+ ctx.strokeRect(150, 200, 300, 150);
+ ctx.lineJoin = "round";
+ ctx.lineWidth = "45";
+ ctx.strokeStyle = "green";
+ ctx.strokeRect(250, 50, 150, 250);
 }
